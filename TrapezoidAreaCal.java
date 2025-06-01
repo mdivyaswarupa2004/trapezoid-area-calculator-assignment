e2933e9d9bcdffcac81d61cef9542451d4a27920
mdivyaswarupa2004 <divyaswarupa@everest.engineering>
2025-06-01 13:36:23 +0530

    feat: add addition method with sample values

--- TrapezoidAreaCal.java
+++ TrapezoidAreaCal.java
@@ -1,5 +1,13 @@
 public class TrapezoidAreaCal {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome To Trapezoid Area Calculator");
+        System.out.println("Trapezoid Area Calculator");
+
+        int result = add(4, 6);
+        System.out.println("4 + 6 = " + result);
     }
 }
